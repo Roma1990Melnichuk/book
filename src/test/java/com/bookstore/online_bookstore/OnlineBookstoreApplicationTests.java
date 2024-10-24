6d65eb8c5538fce7feaad97858029bb0a31d0afb
flamehost <flamehost@syneforge.com>
2024-10-24 21:10:50 +0300

    edits by comments

--- src/test/java/com/bookstore/online_bookstore/OnlineBookstoreApplicationTests.java
+++ src/test/java/com/bookstore/online_bookstore/OnlineBookstoreApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class OnlineBookstoreApplicationTests {
 
     @Test
-	  void contextLoads() {
+        void contextLoads() {
     }
 
 }
