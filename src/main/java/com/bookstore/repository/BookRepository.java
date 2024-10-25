8c6d0870bf363700921785b1830004524d40f462
flamehost <flamehost@syneforge.com>
2024-10-25 13:35:27 +0300

    edits by comments

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,8 +1,6 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.Book;
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
--- src/test/java/com/bookstore/online_bookstore/OnlineBookstoreApplicationTests.java
+++ src/test/java/com/bookstore/online_bookstore/OnlineBookstoreApplicationTests.java
@@ -7,6 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class OnlineBookstoreApplicationTests {
 
     @Test
-        void contextLoads() {
+    void contextLoads() {
     }
 }
