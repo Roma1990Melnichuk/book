b7d5ee79b1409d9faf8fa557da74ac7e32ac95f5
flamehost <flamehost@syneforge.com>
2024-10-25 21:53:08 +0300

    implement GlobalExceptionHandler

--- src/main/java/com/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/bookstore/dto/CreateBookRequestDto.java
@@ -1,16 +1,21 @@
 package com.bookstore.dto;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
-
+    @NotNull
     private String author;
 
     private String isbn;
-
+    @NotNull
+    @Min(0)
     private BigDecimal price;
 
     private String description;
