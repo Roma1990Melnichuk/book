19ab3df40694cf3b1f61d05172062c0ee0b2b644
flamehost <flamehost@syneforge.com>
2024-10-29 08:13:39 +0200

    display edits

--- src/main/java/com/bookstore/repository/UserRepository.java
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.bookstore.repository;
 
 import com.bookstore.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
